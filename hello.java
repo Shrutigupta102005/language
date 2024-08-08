bcf9d6fcd2ef3ca23e3881c273165e8316d90c8a
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-08 10:51:06 +0530

    .

--- hello.java
+++ hello.java
@@ -20,6 +20,32 @@ CHAR SET a-z A-Z 0-9
 s           pecial char @#.,[]
 java tokens(words) - reserved words  50
 identifiers -- users defined names 
-operators +-*/
+operators +-
+                     java naming conventions for identifiers 
+classs name
+       1 must start with capital letter
+       2 if there are multiple words each word must be start with capital letter 
+       3 spaces are not allowed  but we can use underscore .
+       4 upper camel casing 
+method /function
+        1 methods name are in small letter but if there are multiple words each word from second onward should start with capital 
+        2 must not be a keywords
+        3 spaces are not allowed 
+variable name 
+        1 must not be a keyword 
+        2 all letters mus tbe small
+        3first letter must not be digit 
+        4 spaces are not allowed  but we can use underscore .
+        5 if all letters are capital in variable name this variable is constant variable in java also known as final .
+datatype and variable 
+        1variables are placeholders for values in programs and they must be a valid identifers
+        2all variablees must be declared with the suitable dtypes 
+        3dtype specify the type and raange of values to be stored in a variable 
+limit 
+  int 4               float 4               char 2
+  long 8               double 8              bool (1,0)
+  short 2
+  byte 1
+*/
 
- */
+ 
