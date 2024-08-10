017a77fec38fbdf459ec5fb51ec543552bab90b9
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-10 23:21:49 +0530

    all about methods

---
+++ methods.java
@@ -0,0 +1,23 @@
+public class methods {
+    // methods -- block of code which only runs when it is called.also known as functions
+    //static means that the method belongs to the Main class and not an object of the Main class.
+
+    //creating a method 
+    public static int shruti(String fname)
+        {
+            return 10 ;
+        }
+    public static void main(String[] args){   
+    // calling a method 
+    int date = shruti();
+    System.out.println(date);
+    //arguments and parameter 
+   /*
+    Parameters are the placeholders in a method or function definition. They represent the input values that the method expects.
+Arguments are the actual values passed to a method when it’s called. They fill in those placeholders. */ 
+   
+// method overridding -- multiple methods can have same name or return type but diff number or type of parameters is called method overridding .
+
+}
+
+}
