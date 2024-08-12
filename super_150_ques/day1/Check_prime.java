83f917acb1cf3c35763c850a9bf8fb696e0c2167
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-12 22:58:56 +0530

    .

---
+++ super_150_ques/day1/Check_prime.java
@@ -0,0 +1,6 @@
+public class check_prime{
+    public static void main(String[] args{}){
+        int n = 6;
+        
+    }
+}
---
+++ super_150_ques/day1/comparing_3_numbers.java
@@ -0,0 +1,30 @@
+package super_150_ques.day1;
+
+public class comparing_3_numbers {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 3;
+        int c = 5;
+        // for largest number among three
+        if (a>b && a>c)
+        {System.out.println(a  + " is largest ");}
+        else if( b>a && b>c)
+        {System.out.println(b+"is largest");}
+        else
+        {System.out.println(c+"is largest");}
+        // for smallest number among three
+        if (a<b && a<c)
+        {System.out.println(a  + " is smallest ");}
+        else if( b<a && b<c)
+        {System.out.println(b+"is smallest");}
+        else
+        {System.out.println(c);}
+        //for finding the middle term
+        if (a>b && b<c)
+        {System.out.println(b );}
+        else if( b>a && a<c)
+        {System.out.println(a);}
+        else
+        {System.out.println(c);}
+    }
+}
