9f073f78edcd19c85a20d904e8fbb228e23ce50f
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-12 23:04:07 +0530

    .

---
+++ super_150_ques/day1/odd_even.java
@@ -0,0 +1,17 @@
+package super_150_ques.day1;
+
+public class odd_even {
+    public static void main(String[] args)
+    {
+        int num1 = 39;
+        if (num1%2==0)
+        {
+            System.out.println("number is even");
+        }
+        else
+            {
+                System.out.println("number is odd");
+            }
+    }
+    
+}
