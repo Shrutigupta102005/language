25741540654c1ea55b9697a03eb0675d632060c9
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-20 21:06:00 +0530

    .

--- super_150_ques/day2/Pattern5.java
+++ super_150_ques/day2/Pattern5.java
@@ -1,10 +1,14 @@
 public class Pattern5 {
     int n = 5 ;
-    for(int i =0;i < 2n;i++){
-        for(int j =0; j;j++)
-        {System.out.print("* ");
-    }
-    System.out.println();
+    for (int i = 0 ; i < (2*n - 1);i++){
+        if (i>n)
+            {}
+        else 
+            {
+                for(int j = 0;j<i;j++)
+                {System.out.print("* ");}
+                System.out.println( );
+            }
     }
 }
 /*
