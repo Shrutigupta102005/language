8e903f19dbee14a3fdf86d8912336ee63b297808
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-22 10:26:44 +0530

    all about naming conventions

---
+++ java basics/naming_convention.java
@@ -0,0 +1,10 @@
+//               naming convenctions 
+//java follows camel casing(myNameIs)  not snake casing (my_name_is)
+/*
+ * class - first word capital(Calc)
+ * variables and methods - marks,show()
+ * constants - all words capital (SHRUTI)
+ * constructors -  Human() first word capital then round brackets 
+ * 
+ * if we ahve combined words use cameel casing .
+ */
