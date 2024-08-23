1b356bd4dd3e5b4b7bd96b0bd50ff5200eb46c8d
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-23 07:21:14 +0530

    .

--- ques_class/pw_35/ques10.java
+++ ques_class/pw_35/ques10.java
@@ -3,7 +3,11 @@ package pw_35;
 public class ques10 {
     public static void main(String[] args)
     {
-        
+       int year = 2023;
+       if (year%4==0&&year%100!=0 || year%400==0)
+       {System.out.println("This is an leap year!!");} 
+       else
+       {System.out.println("this is not an leap year!!");}
     }
     
 }
