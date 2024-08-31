f40c990c3c0361b71de4614e749d9be793d64564
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-08-31 19:22:15 +0530

    sum of two number

--- java basics/methods.java
+++ java basics/methods.java
@@ -1,23 +1,17 @@
-public class methods {
-    // methods -- block of code which only runs when it is called.also known as functions
-    //static means that the method belongs to the Main class and not an object of the Main class.
+import java.util.Scanner;
+public class methods{
+    public static void main(String[] args){
+         int d= sum();
+         System.out.println(d);
+    }
+    static int sum(){
+        Scanner my_obj = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a = my_obj.nextInt();
+        System.out.println("enter second number");
+        int b = my_obj.nextInt();
+        int c = a+b;
+        return c;
 
-    //creating a method 
-    public static int shruti(String fname)
-        {
-            return 10 ;
-        }
-    public static void main(String[] args){   
-    // calling a method 
-    int date = shruti();
-    System.out.println(date);
-    //arguments and parameter 
-   /*
-    Parameters are the placeholders in a method or function definition. They represent the input values that the method expects.
-Arguments are the actual values passed to a method when it’s called. They fill in those placeholders. */ 
-   
-// method overridding -- multiple methods can have same name or return type but diff number or type of parameters is called method overridding .
-
-}
-
+    }
 }
