e86e2e941f408c1ee573cc26e5aec7510f6b40f2
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-09-03 23:32:59 +0530

    .

--- oops/oops1.java
+++ oops/oops1.java
@@ -1,34 +1,35 @@
 package oops;
 
-public class oops1{
-    // class creation 
+public class oops1 {
+    // Inner class creation
     public class student {
-      int rollno ;
-      String name ;
-      int marks;
+        int rollno;
+        String name;
+        int marks;
+
+        // Constructor for the student class
+        public student(int rollno, String name, int marks) {
+            this.rollno = rollno;
+            this.name = name;
+            this.marks = marks;
+        }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         oops1 outerInstance = new oops1();
-        // object creation 
-        oops1.student student1 = outerInstance.new student();
-        oops1.student student2 = outerInstance.new student();
-        oops1.student student3 = outerInstance.new student();
-        // maniuplation of object 
-        student1.rollno = 1;
-        student1.name = "dhruv";
-        student1.marks = 10;
 
-        student2.rollno = 2;
-        student2.name = "shubh";
-        student2.marks = 20;
+        // Create instances of the inner class
+        oops1.student student1 = outerInstance.new student(1, "dhruv", 10);
+        oops1.student student2 = outerInstance.new student(2, "shubh", 20);
+        oops1.student student3 = outerInstance.new student(3, "shruti", 30);
 
-        student3.rollno = 3;
-        student3.name = "shruti";
-        student3.marks = 30;
-        System.out.println(student2.name);
-        // mainuplation of object using constructor 
-        oops1.student student4 = outerInstance.new student(4,"ajay",55);
-        
+        // Print student details
+        System.out.println("Student 1: Roll No=" + student1.rollno + ", Name=" + student1.name + ", Marks=" + student1.marks);
+        System.out.println("Student 2: Roll No=" + student2.rollno + ", Name=" + student2.name + ", Marks=" + student2.marks);
+        System.out.println("Student 3: Roll No=" + student3.rollno + ", Name=" + student3.name + ", Marks=" + student3.marks);
+
+        // Create an instance of the inner class using the constructor
+        oops1.student student4 = outerInstance.new student(4, "ajay", 55);
+        System.out.println("Student 4: Roll No=" + student4.rollno + ", Name=" + student4.name + ", Marks=" + student4.marks);
     }
 }
-/* */
