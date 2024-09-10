c96afd2d32ab72deec49d9743439995fb4e9e89e
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-09-10 20:19:13 +0530

    .

--- ques_class/pw_35/ques7.java
+++ ques_class/pw_35/ques7.java
@@ -1,13 +1,13 @@
 package pw_35;
-
+import java.util.Arrays;
 public class ques7 {
     public static void main(String[] args)
     {
-        int num1 = 0b0101;
-        int num2 = 0b1111;
-        int c = num1+num2;
-        System.
-        
+        int [] arr = { 2,3,4,5,6};
+        for(int i = arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+     System.out.println(Arrays.toString(arr));   
     }
     
 }
