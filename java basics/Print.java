357b07a9212c6f255cd384e521ed97970ed6708b
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-09-12 13:18:30 +0530

    abt  format specifer in java

---
+++ java basics/Print.java
@@ -0,0 +1,15 @@
+public class Print {
+    public static void main(String[] args){
+        String name = "shruti";
+        int age = 19;
+        System.out.printf("my name is %s and my age is %d",name,age);
+        System.out.printf("my name is %s and my age is %d",name,age);
+        System.out.println(name);   // /n will be added in the new line 
+        System.out.print(age); // will not end /n in the new line 
+
+    }
+}
+// %d -- int 
+// %f -- float
+// %c -- char
+// %s -- string
