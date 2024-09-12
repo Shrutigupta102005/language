48ce9dad4478fac6970c70409b751c0ce55c58f1
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-09-12 13:10:24 +0530

    about strings

--- java basics/str_ings.java
+++ java basics/str_ings.java
@@ -1,10 +1,17 @@
 public class str_ings {
-    // basically an object of string class.
-    // array of char string works same as string
-    /*                 string
-     * sezlizable       comparable               charsequence (interface)
-     *                                         String, StringBuffer and StringBuilder
-     * 
-     * 
-     */
+    public static void main(String[] args) {
+        // method 1
+        String name = new String("shruti");
+        System.out.println(name);
+
+        // method 2 
+        String age = "seven";
+        System.out.println(age );
+        
+    }
 }
+// string -- a sequence of char is called string 
+/*it is immutable
+ * 
+ * string is an object of class but since it is used many times it can be accesed as a datatype
+ */
