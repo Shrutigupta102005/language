8105b6c439c0cede30a023f19ea7db1b16adbabb
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-09-19 00:20:16 +0530

    .

---
+++ mathbasic/alldivisors.java
@@ -0,0 +1,12 @@
+package mathbasic;
+
+public class alldivisors {
+    public static void main(String[] args){
+        int num =10;
+        for(int i =1; i<=num ;i++){
+            if(num%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
