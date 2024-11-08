1808e82ace7b011442e38143e83ad6b7d243bdf8
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-11-08 11:59:43 +0530

    .

--- LinearSearch/search2darray.java
+++ LinearSearch/search2darray.java
@@ -1,10 +1,28 @@
 package LinearSearch;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class search2darray {
     public static void main(String[] args) {
-        
+        int[][] arr ={{1,2,3},
+                      {3,64,5},
+                      {6,7,8}};
+        int target = 6 ; // search for row number as well as column number  
+        int[] ans = search(arr,target);
+        System.out.println(Arrays.toString(ans));
     }
-    static void search(){
+    static int[] search(int[][] arr , int target){
         
+        for(int i = 0;i<arr.length;i++){
+            for(int j =0;j<arr[i].length;j++){
+                if (target == arr[i][j]){
+                    int[] rel ={i,j};
+                    return rel;
+                    
+                }
+            }
+        }
+        return new int[] {-1,-1};
     }
 }
