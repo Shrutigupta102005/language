5809e190dfcdb9b34f7d28d541ffbe4963050ebc
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2024-11-12 09:31:41 +0530

    .

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,8 +1,28 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     // with every pass the last element and so on get on its poisition 
     public static void main(String[] args) {
-        
+        int[] arr ={4,5,3,2,1};
+        bubblesort(arr); 
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubblesort(int[] arr){
+        boolean swaped = false;
+        for(int i = 0;i<arr.length;i++){
+            for(int j =1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp ;
+                    swaped = true;
+                }
+            }
+            if( swaped == false){
+                break;
+            }
+        }
     }
 }
