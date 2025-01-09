80b2cca711b49b817d092ed39f8613bb471834d2
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-09 19:42:08 +0530

    ..

--- Recursion/namen.java
+++ Recursion/namen.java
@@ -1,5 +1,15 @@
 package Recursion;
 
 public class namen {
-    
+    public static void main(String[] args){
+        name(1,5);
+    }
+    static void name( int i , int n){
+        if(i>n){
+            return;
+        }
+        System.out.println("Shruti gupta");
+        
+        name(i+1,n);
+    }
 }
