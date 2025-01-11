e2c19a11b1a0bfd2fd95e31822212f8d93391f55
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-11 16:47:02 +0530

    ..

--- String/Lexographical_order.java
+++ String/Lexographical_order.java
@@ -5,5 +5,23 @@ public class Lexographical_order {
         String s1 = "Komal" ;
         String  s2 = "Kunal";
         System.out.println(s1.compareTo(s2));
+        int a = mera(s1,s2);
+        System.out.println(a);
+        
+       
     }
-}
+    // to comapre too mera khud ka 
+    static int mera(String s1,String s2){
+        if(s1==s2){
+            return 0;
+        }
+        int len = Math.min(s1.length(),s2.length());
+        for(int i =0;i<len;i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                return s1.charAt(i)-s2.charAt(i);
+            }
+        }
+        return s1.length()-s2.length();
+    }
+        
+ }
