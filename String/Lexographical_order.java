2f5931c8c98feaec3a559e6a59934faa1edfa8de
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-11 16:33:36 +0530

    ..

--- String/Lexographical_order.java
+++ String/Lexographical_order.java
@@ -2,7 +2,7 @@ package String;
 
 public class Lexographical_order {
     public static void main(String[] args){
-        String s1 = "komal" ;
+        String s1 = "Komal" ;
         String  s2 = "Kunal";
         System.out.println(s1.compareTo(s2));
     }
