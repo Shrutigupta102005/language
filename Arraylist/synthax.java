f694eca6a1b7529ad43f6dc0886762f2a16722e7
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-13 00:30:00 +0530

    arraylist

---
+++ Arraylist/synthax.java
@@ -0,0 +1,41 @@
+package Arraylist;
+import java.util.*;
+public class synthax {
+    public static void main(String[] args){
+        //Creation 
+        ArrayList<Integer> list1 = new ArrayList<>();
+        // add element  range 0 to size 
+        list1.add(20);
+        list1.add(20);
+        list1.add(20);
+        list1.add(3,45);
+        System.out.println(list1);
+        // element get  range 0  to size -1 
+        System.out.println(list1.get(3));
+        //element remove 
+        list1.remove(2);                       
+        System.out.println(list1);                                                                             
+        //element update
+        System.out.println(list1.set(1,56));   
+        System.out.println(list1);   
+        // element sort 
+        Collections.sort(list1);
+        System.out.println(list1);   
+        //display arraylist 
+        ArrayList<String> ll = new ArrayList<>();
+        ll.add("shruti");
+        ll.add("shubh");
+        ll.add("dhruv");
+        ll.add("ajay");
+        ll.add("ruby");
+        System.out.println(ll);
+        // display with loop 
+        for(int i =0 ; i < ll.size();i++){
+            System.out.println(ll.get(i));
+        }
+        // for - each loop  
+        for(int val:list1){
+            System.out.println(val);
+        }
+    }
+}
