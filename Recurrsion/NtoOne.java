18929916e8b0243f2d07bce1f96607d2250f48e5
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-18 19:22:49 +0530

    ..

--- Recurrsion/NtoOne.java
+++ Recurrsion/NtoOne.java
@@ -3,13 +3,32 @@ package Recurrsion;
 public class NtoOne {
         public static void main(String[] args){
             int n =5;
-            fun(n);
+    
+            System.out.println(fact(5,1));
     }
     static void fun(int n){
         if(n == 0){
             return;
         }
-        System.out.println(n);
+        
         fun(n-1);
+        System.out.println(n);
+    }
+    static int pow(int a,int b){
+        if(b==0){
+            return 1;
+        }
+        int ans = pow(a,b-1);
+        return a*ans;
+    }
+
+    // tailed recursion 
+    // headed recursion 
+
+    static int fact(int n,int ans){
+        if(n==0){
+            return ans;
+        }
+        return fact(n-1,ans*n);
     }
 }
