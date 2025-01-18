caaa8badfe0379204d0474ef59a369a3ba4f6842
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-18 19:57:39 +0530

    ...

--- Recurrsion/First_occurence.java
+++ Recurrsion/First_occurence.java
@@ -1,5 +1,17 @@
 package Recurrsion;
 
 public class First_occurence {
-    
+    public static void main(String[] args){
+        int[] arr = { 2,3,4,5,56,6,7,5,6};
+        int target = 5;
+        int ans = foo(arr,target,0);
+        System.out.println(ans);
+    }
+    static int foo(int[] arr , int target , int index){
+        if(arr[index]== target){
+            return index;
+        }
+        return foo(arr ,target ,index+1);
+    }
 }
+
