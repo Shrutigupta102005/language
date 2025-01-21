271afa032a2b7eaba7665dd4373287519b11aaf4
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-21 09:07:58 +0530

    ..

--- Recurrsion/coin.java
+++ Recurrsion/coin.java
@@ -2,6 +2,16 @@ package Recurrsion;
 
 public class coin {
     public static void main(String[] args){
-        
+        int n = 3;
+        coin(n,"");
     }
+    static void coin(int n , String ans){
+        if(n==0){
+            System.out.println(ans);
+            return ; 
+        }
+        coin(n-1,ans+'T');
+        coin(n-1,ans+'H');
+    }
+    
 }
