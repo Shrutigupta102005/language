537eb4129af21adec22fed4a13b772e0fe29ecb6
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-01-22 19:14:04 +0530

    ...

--- oops/basics.java
+++ oops/basics.java
@@ -6,7 +6,9 @@ public class basics {
         Student shruti = new Student() ;
         System.out.println(shruti.name);
         System.out.println(shruti.age);
+        shruti.name = "Shruti";
         shruti.intro_yourself();
+        shruti.intro_yourself("dhruv");
     }
     // creation of a class
     public static class Student{
@@ -14,7 +16,12 @@ public class basics {
         int age;
 
         public void intro_yourself(){
-            System.out.println("hey wait this is a function or you can call me a method which is made by  shruti gupta understand ....");
+            System.out.println("hey wait this is a function or you can call me a method which is made by  shruti gupta understand ...."+name);
         }
+        // use of this keyword
+        public void intro_yourself(String name){
+            System.out.println("hey wait this is a function or you can call me a method which is made by  shruti gupta understand ...."+name+this.name);
+        }
+
     }
 }
