c26abd7ba09615686c06aa57b80254a4ed324fd0
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-02-05 16:24:07 +0530

    ..

--- Recurrsion/sudoko.java
+++ Recurrsion/sudoko.java
@@ -7,7 +7,12 @@ public class sudoko {
 				{ 0, 0, 3, 0, 1, 0, 0, 8, 0 }, { 9, 0, 0, 8, 6, 3, 0, 0, 5 }, { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
 				{ 1, 3, 0, 0, 0, 0, 2, 5, 0 }, { 0, 0, 0, 0, 0, 0, 0, 7, 4 }, { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
 		Print(grid, 0, 0);
-	
+        for(int i = 0; i <grid.length;i++){
+            for(int j =0;j < grid[0].length;j++){
+                System.out.print(grid[i][j]+"");
+            }
+            System.out.println();
+        }
 
 	}
 
