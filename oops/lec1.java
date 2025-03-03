be7e8b041f27ee265737bf90e42cbc5ad39f6310
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-03-03 22:00:33 +0530

    oops lec1

--- oops/lec1.java
+++ oops/lec1.java
@@ -1,9 +1,22 @@
 public class lec1 {
     public static void main(String[] args) {
-        student s = new student();
+        student s = new student(); // object creation
+        s.name = "shruti"; // setting the value
+        s.age = 18;
+        s.Intro_yourself();
+        student s1 = new student(); // object creation
+        s1.name = "dhruv"; // setting the value
+        s1.age = 19;
+        s1.Intro_yourself();
+       
     }
-    public class student{
-        int name = "shruti";
-
+    
+    public static class student { 
+        String name;
+        int age;
+        
+        public void Intro_yourself() {
+            System.out.println("My name is " + name + " and age is " + age); // Fixed missing spaces and removed extra parenthesis
+        }
     }
 }
