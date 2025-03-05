3de61c490db36aac6bc5f16dd7291d5b86915de0
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-03-05 15:00:33 +0530

    Stack_reverse

---
+++ Stacks/SR.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class SR {
+    public static void main(String[] args){
+        Stack <Integer> st = new Stack<>();
+        if(st.isEmpty()){
+            st.push(item);
+            return;
+        }
+        int x = s.pop();
+        Add_Last(s,item);
+        s.push(x);
+    }
+}
--- Stacks/Stack_creation.java
+++ Stacks/Stack_creation.java
@@ -14,10 +14,34 @@ public class Stack_creation {
         st.push(90);
         st.push(100);
         st.push(20);
+        //Add_Last(st,6);
         System.out.println(st.peek());
         System.out.println(st.size());
         System.out.println(st.pop());
         System.out.println(st.peek());
         System.out.println(st.capacity());
+        Add_Last(st,6);
+        System.out.println(st);
+        Stack_reverse(st,54);
+        System.out.println(st);
+        //without using extra stack --> use the approach of recurssion
+        
+    }
+    public static void Add_Last(Stack<Integer> st,int item){
+        if(st.isEmpty()){
+            st.push(item);
+            return;
+        }
+        int x = st.pop();
+        Add_Last(st,item);
+        st.push(x);
+    }
+    public static void Stack_reverse(Stack<Integer> st,int item){
+        if(st.isEmpty()){
+            return;
+        }
+        int x = st.pop();
+        Stack_reverse(st,item);
+        Add_Last(st,x);
     }
 }
