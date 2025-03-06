2ce72d694e99067ebaa5c626c47a9b6e5a69d090
Shrutigupta102005 <156588272+Shrutigupta102005@users.noreply.github.com>
2025-03-06 15:08:50 +0530

    Stack_span coding block question

--- oops/Stack_span.java
+++ oops/Stack_span.java
@@ -1,7 +1,28 @@
 package oops;
 
+import java.util.Stack;
+
 public class Stack_span {
     public static void main(String[] args){
-        
+        int [] arr = {30, 35 , 40 ,38,35};
+        Cal_Stack(arr);
+    }
+    public static void Cal_Stack(int[] arr){
+        int [] ans = new int [arr.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i =0; i <ans.length ; i++){
+            while(!st.isEmpty() && arr[i] > arr[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i] = i+1;
+            }else{
+                ans[i] = i - st.peek();
+            }
+            st.push(i);
+        }
+        for(int i = 0 ; i <ans.length;i++){
+            System.out.println(ans[i] + " ");
+        }
     }
 }
